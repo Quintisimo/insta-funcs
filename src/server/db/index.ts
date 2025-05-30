5e3eb4e06c4d34d4c779db60d92b4d3ef8e7bffb
Quintus Cardozo <qcardozo@getlegaltech.com>
2025-05-31 02:52:56 +1000

    chore: switch to better-sqlite3 from libsql

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,14 @@
-import { drizzle } from "drizzle-orm/libsql";
+import { createRequire } from "node:module";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+const require = createRequire(import.meta.url);
+
 import * as schema from "./schema";
 
-export const dbUrl = "file:./data/db.sqlite";
+const Database = require("better-sqlite3");
 
-export const db = drizzle(dbUrl, { schema });
+export const dbUrl = import.meta.env.PROD
+  ? "../data/db.sqlite"
+  : "./data/db.sqlite";
+
+const sqlite = new Database(dbUrl);
+export const db = drizzle({ client: sqlite, schema });
